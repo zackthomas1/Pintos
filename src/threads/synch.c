4aa1a85f49a62bbe9d1c1f516a4a063b18348b20
Zachary Thomas <zachat1@circinus-18.ics.uci.edu>
2024-03-07 00:05:21 -0800

    Minor change to cond_signal function for code clarity.

--- src/threads/synch.c
+++ src/threads/synch.c
@@ -321,10 +321,8 @@ cond_signal (struct condition *cond, struct lock *lock UNUSED)
   ASSERT (lock_held_by_current_thread (lock));
 
   if (!list_empty (&cond->waiters)) 
-
     list_sort(&cond->waiters, cond_priority_greater, NULL); 
-    sema_up (&list_entry (list_pop_front (&cond->waiters),
-                          struct semaphore_elem, elem)->semaphore);
+  sema_up (&list_entry (list_pop_front (&cond->waiters), struct semaphore_elem, elem)->semaphore);
 }
 
 /* Wakes up all threads, if any, waiting on COND (protected by
